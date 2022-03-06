165e4a4306cf3fb713c21e64e0aa592cafbe1d39
slawek.krauza <slawek.krauza@gmail.com>
2022-03-06 22:45:47 +0100

    init commit3

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
@@ -11,7 +11,7 @@ import spark.Response;
 import static spark.Spark.*;
 
 // change to be deployed
-// trigger build 2
+// trigger build 3
 public class RxEndpoints {
    public static void initalizeEndpoints() {
 
